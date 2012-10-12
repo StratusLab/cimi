fc2caeb60395115e6e7a968e37947bd237d5b858
Charles Loomis <cal@sixsq.com>
2012-10-12 08:48:29 +0200

    complete the filter grammar; add tests for parser

--- filter-parser/src/test/java/eu/stratuslab/cimi/CIMIFilterParserTest.java
+++ filter-parser/src/test/java/eu/stratuslab/cimi/CIMIFilterParserTest.java
@@ -16,27 +16,151 @@
 
 package eu.stratuslab.cimi;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import java.io.StringReader;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.junit.Test;
+
+import org.joda.time.DateTime;
 
 import eu.stratuslab.cimi.ParseException;
 import eu.stratuslab.cimi.SimpleNode;
 
 public class CIMIFilterParserTest {
     
-    private ASTFilter parseFilter(String filter) throws ParseException {
-        CIMIFilterParser parser = new CIMIFilterParser(new StringReader(filter));
-        return parser.filter();
+    private CIMIFilterParser parser(String s) {
+        return new CIMIFilterParser(new StringReader(s));
     }
     
     @Test
-	public void testValidFilters() throws ParseException {
+    public void testValidFilters() throws ParseException {
         
-        // Check empty statement.
-        SimpleNode node = parseFilter("alpha=3 & beta=4");
-        node.dump(">  ");
+        String[] validFilters = {"alpha=3",
+                                 "3=alpha",
+                                 "alpha=3 and beta=4",
+                                 "3=alpha and 4=beta",
+                                 "(alpha=3)",
+                                 "(3=alpha)",
+                                 "property['beta']=4",
+                                 "4=property['beta']",
+                                 "alpha=3 and beta=4",
+                                 "alpha=3 or beta=4",
+                                 "alpha=3 and beta=4 or gamma=5 and delta=6"};
         
+        for (String filter : validFilters) {
+            System.out.println(filter);
+            SimpleNode n = parser(filter).filter();
+            n.dump("> ");
+        }
     }
-    
+
+    @Test
+    public void testValidIntegers() throws ParseException {
+
+        String[] validIntegers = {"0", "00", "01", "1", "123", "9999"};
+        
+        for (String s : validIntegers) {
+            Integer correct = Integer.valueOf(s);
+            Integer i = parser(s).intValue();
+            assertEquals(correct, i);
+        }
+    }
+
+    @Test
+    public void testInvalidIntegers() throws ParseException {
+
+        String[] invalidIntegers = {"", "abc"};
+        
+        for (String s : invalidIntegers) {
+            try {
+                parser(s).intValue();
+                fail("invalid integer did not throw an exception: " + s);
+            } catch (ParseException e) {
+                // OK
+            }
+        }
+    }
+
+    @Test
+    public void testValidBooleans() throws ParseException {
+
+        String[] validBooleans = {"true", "false"};
+        
+        for (String s : validBooleans) {
+            Boolean correct = Boolean.valueOf(s);
+            Boolean b = parser(s).boolValue();
+            assertEquals(correct, b);
+        }
+    }
+
+    @Test
+    public void testStrings() throws ParseException {
+
+        Map<String, String> cases = new HashMap<String, String>();
+        cases.put("'alpha'", "alpha");
+        cases.put("\"alpha\"", "alpha");
+
+        cases.put("'al\\\\pha'", "al\\pha");
+        cases.put("\"al\\\\pha\"", "al\\pha");
+        cases.put("'al\\'pha'", "al'pha");
+        cases.put("\"al\\\"pha\"", "al\"pha");
+
+        cases.put("'\\\\pha'", "\\pha");
+        cases.put("\"\\\\pha\"", "\\pha");
+        cases.put("'\\'pha'", "'pha");
+        cases.put("\"\\\"pha\"", "\"pha");
+        
+        cases.put("'al\\\\'", "al\\");
+        cases.put("\"al\\\\\"", "al\\");
+        cases.put("'al\\''", "al'");
+        cases.put("\"al\\\"\"", "al\"");
+
+        for (Map.Entry<String, String> entry : cases.entrySet()) {
+            String correct = entry.getValue();
+            String s = parser(entry.getKey()).stringValue();
+            assertEquals(correct, s);
+        }
+    }
+
+    @Test
+    public void testValidDates() throws ParseException {
+
+        String[] validDates = {"2012-01-02",
+                               "2012-01-02T13:14:25.6Z",
+                               "2012-01-02T13:14:25.6-01:15",
+                               "2012-01-02T13:14:25.6+02:30"};
+        
+        for (String s : validDates) {
+            DateTime correct = DateTime.parse(s);
+            DateTime i = parser(s).dateValue();
+            assertEquals(correct, i);
+        }
+    }
+
+    @Test
+    public void testInvalidDates() throws ParseException {
+
+        String[] invalidDates = {"2012",
+                                 "2012-01-99T13:14:25.6Z",
+                                 "2012-01-99T13:14:25.6Q",
+                                 "2012-01:02T25:14:25.6-01:15",
+                                 "2012-01-02T13:14:25.6+02-30"};
+        
+        for (String s : invalidDates) {
+            try {
+                parser(s).dateValue();
+                fail("invalid date did not throw an exception: " + s);
+            } catch (ParseException e) {
+                // OK
+            }
+        }
+    }
+
+
 }
