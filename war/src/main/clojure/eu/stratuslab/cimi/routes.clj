9b97ead126818f8d75be3e55fcf3f35eff6b61bd
Charles Loomis <cal@sixsq.com>
2013-08-08 17:42:55 +0200

    remove unnecessary ns references

--- war/src/main/clojure/eu/stratuslab/cimi/routes.clj
+++ war/src/main/clojure/eu/stratuslab/cimi/routes.clj
@@ -1,10 +1,7 @@
 (ns eu.stratuslab.cimi.routes
   "Primary routing table for CIMI application."
-  (:require [clojure.tools.logging :refer [info warn]]
-            [compojure.core :refer :all]
+  (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [compojure.handler :as handler]
-            [compojure.response :as response]
             [eu.stratuslab.cimi.resources.cloud-entry-point :as cep]))
 
 (defroutes main-routes
