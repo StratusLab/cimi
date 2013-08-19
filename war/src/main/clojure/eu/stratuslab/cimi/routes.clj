a8d2e01fac19f8dca185983813d5f858011eccc7
Charles Loomis <cal@sixsq.com>
2013-08-19 15:19:26 +0200

    add the job and volume routes to server

--- war/src/main/clojure/eu/stratuslab/cimi/routes.clj
+++ war/src/main/clojure/eu/stratuslab/cimi/routes.clj
@@ -3,10 +3,14 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [eu.stratuslab.cimi.resources.cloud-entry-point :as cep]
+            [eu.stratuslab.cimi.resources.volume :as volume]
+            [eu.stratuslab.cimi.resources.job :as job]
             [eu.stratuslab.cimi.resources.machine-configuration :as mc]))
 
 (defroutes main-routes
   cep/resource-routes
   mc/resource-routes
+  job/resource-routes
+  volume/resource-routes
   (GET "/debug" {:as req} {:body req})
   (route/not-found "Page not found"))
