1a0e22b63df3c9b059dbed72ef50d607578af002
Charles Loomis <cal@sixsq.com>
2014-08-06 14:20:19 +0200

    lifecycle working for machine images

--- jar/src/main/clojure/eu/stratuslab/cimi/resources/machine_image.clj
+++ jar/src/main/clojure/eu/stratuslab/cimi/resources/machine_image.clj
@@ -17,20 +17,13 @@
 (ns eu.stratuslab.cimi.resources.machine-image
   "Utilities for managing the CRUD features for machine images."
   (:require
-    [clojure.string :as str]
-    [couchbase-clj.client :as cbc]
-    [couchbase-clj.query :as cbq]
-    [clojure.data.json :as json]
     [eu.stratuslab.cimi.resources.impl.common :as c]
     [eu.stratuslab.cimi.resources.utils.utils :as u]
     [eu.stratuslab.cimi.resources.utils.auth-utils :as a]
-    [eu.stratuslab.cimi.cb.views :as views]
-    [compojure.core :refer [defroutes let-routes GET POST PUT DELETE ANY]]
-    [ring.util.response :as r]
     [cemerick.friend :as friend]
     [schema.core :as s]
     [eu.stratuslab.cimi.resources.impl.common :as c]
-    [clojure.tools.logging :as log]))
+    [eu.stratuslab.cimi.resources.impl.common-crud :as crud]))
 
 (def ^:const resource-tag :machineImages)
 
@@ -42,19 +35,11 @@
 
 (def ^:const collection-uri (str c/cimi-schema-uri collection-name))
 
-(def ^:const base-uri (str c/service-context resource-name))
-
 (def collection-acl {:owner {:principal "::ADMIN"
                              :type      "ROLE"}
                      :rules [{:principal "::USER"
                               :type      "ROLE"
                               :right     "MODIFY"}]})
-
-
-(defn uuid->uri
-  [uuid]
-  (str resource-name "/" uuid))
-
 ;;
 ;; MachineImage schema
 ;;
@@ -71,128 +56,49 @@
           (s/optional-key :imageLocation) c/NonBlankString
           (s/optional-key :relatedImage)  c/ResourceLink}))
 
-(def validate (u/create-validation-fn MachineImage))
+;;
+;; multimethods for validation and operations
+;;
 
-(defn add-cops
-  "Adds the collection operations to the given resource."
-  [resource]
-  (if (a/can-modify? collection-acl)
-    (let [ops [{:rel (:add c/action-uri) :href base-uri}]]
-      (assoc resource :operations ops))
-    resource))
+(def validate-fn (u/create-validation-fn MachineImage))
+(defmethod c/validate resource-uri
+           [resource]
+  (validate-fn resource))
 
-(defn add-rops
-  "Adds the resource operations to the given resource."
-  [resource]
-  (if (a/can-modify? (:acl resource))
-    (let [href (:id resource)
-          ops [{:rel (:edit c/action-uri) :href href}
-               {:rel (:delete c/action-uri) :href href}]]
-      (assoc resource :operations ops))
-    resource))
+(defmethod crud/add-acl resource-name
+           [resource resource-name]
+  (a/add-acl resource (friend/current-authentication)))
 
-(defn add
-  "Add a new MachineImage to the database.  The entry contains
-   the fields for the new MachineImage.  The :id, :resourceURI,
-   :created, and :updated fields will be provided automatically.  The
-   entry will be validated before being added to the database."
-  ([cb-client] (add cb-client {}))
+;;
+;; CRUD operations
+;;
 
-  ([cb-client entry]
-   (let [uri (uuid->uri (u/random-uuid))
-         entry (-> entry
-                   (u/strip-service-attrs)
-                   (assoc :id uri)
-                   (assoc :resourceURI resource-uri)
-                   (u/update-timestamps)
-                   (a/add-acl (friend/current-authentication))
-                   (validate))]
-     (if (cbc/add-json cb-client uri entry)
-       (r/created uri)
-       (r/status (r/response (str "cannot create " uri)) 400)))))
+(def add-impl (crud/get-add-fn resource-name collection-acl resource-uri))
 
-(defn retrieve
-  "Returns the data associated with the requested MachineImage
-   entry (identified by the uuid)."
-  [cb-client uuid]
-  (if-let [json (cbc/get-json cb-client (uuid->uri uuid))]
-    (if (a/can-view? (friend/current-authentication) (:acl json))
-      (r/response (add-rops json))
-      (u/unauthorized))
-    (r/not-found nil)))
+(defmethod crud/add resource-name
+           [request]
+  (add-impl request))
 
-;; FIXME: Implementation should use CAS functions to avoid update conflicts.
-(defn edit
-  "Updates the given resource with the new information.  This will
-   validate the new entry before updating it."
-  [cb-client uuid entry]
-  (let [uri (uuid->uri uuid)]
-    (if-let [current (cbc/get-json cb-client uri)]
-      (if (a/can-modify? (friend/current-authentication) (:acl current))
-        (let [updated (->> entry
-                           (u/strip-service-attrs)
-                           (merge current)
-                           (u/update-timestamps)
-                           (add-rops)
-                           (validate))]
-          (if (cbc/set-json cb-client uri updated)
-            (r/response updated)
-            (r/status (r/response nil) 409)))               ;; conflict
-        (u/unauthorized))
-      (r/not-found nil))))
+(def retrieve-impl (crud/get-retrieve-fn resource-name))
 
-(defn delete
-  "Deletes the named machine image."
-  [cb-client uuid]
-  (let [uri (uuid->uri uuid)]
-    (if-let [current (cbc/get-json cb-client uri)]
-      (if (a/can-modify? (friend/current-authentication) (:acl current))
-        (if (cbc/delete cb-client uri)
-          (r/response nil)
-          (r/not-found nil))
-        (u/unauthorized))
-      (r/not-found nil))))
+(defmethod crud/retrieve resource-name
+           [request]
+  (retrieve-impl request))
 
-(defn query
-  "Searches the database for resources of this type, taking into
-   account the given options."
-  [cb-client & [opts]]
-  (let [principals (a/authn->principals (friend/current-authentication))
-        configs (u/viewable-resources cb-client resource-name principals opts)
-        configs (map add-rops configs)
-        collection (add-cops {:resourceURI collection-uri
-                              :id          base-uri
-                              :count       (count configs)})]
-    (r/response (if (empty? collection)
-                  collection
-                  (assoc collection :machineImages configs)))))
+(def edit-impl (crud/get-edit-fn resource-name))
 
-#_(defroutes collection-routes
-           (POST base-uri {:keys [cb-client body]}
-                 (if (a/can-modify? collection-acl)
-                   (let [json (u/body->json body)]
-                     (add cb-client json))
-                   (u/unauthorized)))
-           (GET base-uri {:keys [cb-client body]}
-                (if (a/can-view? collection-acl)
-                  (let [json (u/body->json body)]
-                    (query cb-client json))
-                  (u/unauthorized)))
-           (ANY base-uri []
-                (u/bad-method)))
+(defmethod crud/edit resource-name
+           [request]
+  (edit-impl request))
 
-#_(def resource-routes
-  (let-routes [uri (str base-uri "/:uuid")]
-              (GET uri [uuid :as {cb-client :cb-client}]
-                   (retrieve cb-client uuid))
-              (PUT uri [uuid :as {cb-client :cb-client body :body}]
-                   (let [json (u/body->json body)]
-                     (edit cb-client uuid json)))
-              (DELETE uri [uuid :as {cb-client :cb-client}]
-                      (delete cb-client uuid))
-              (ANY uri []
-                   (u/bad-method))))
+(def delete-impl (crud/get-delete-fn resource-name))
 
-#_(defroutes routes
-           collection-routes
-           resource-routes)
+(defmethod crud/delete resource-name
+           [request]
+  (delete-impl request))
+
+(def query-impl (crud/get-query-fn resource-name collection-acl collection-uri collection-name resource-tag))
+
+(defmethod crud/query resource-name
+           [request]
+  (query-impl request))
---
+++ jar/src/test/clojure/eu/stratuslab/cimi/resources/machine_image_lifecycle_test.clj
@@ -0,0 +1,106 @@
+(ns eu.stratuslab.cimi.resources.machine-image-lifecycle-test
+  (:require
+    [eu.stratuslab.cimi.resources.machine-image :refer :all]
+    [eu.stratuslab.cimi.resources.utils.utils :as u]
+    [eu.stratuslab.cimi.couchbase-test-utils :as t]
+    [clojure.test :refer :all]
+    [clojure.data.json :as json]
+    [peridot.core :refer :all]
+    [eu.stratuslab.cimi.routes :as routes]
+    [eu.stratuslab.cimi.resources.impl.common :as c]))
+
+(use-fixtures :each t/flush-bucket-fixture)
+
+(use-fixtures :once t/temp-bucket-fixture)
+
+(def ^:const base-uri (str c/service-context resource-name))
+
+(defn ring-app []
+  (t/make-ring-app (t/concat-routes routes/final-routes)))
+
+(def valid-acl {:owner {:principal "::ADMIN"
+                        :type      "ROLE"}
+                :rules [{:principal "::USER"
+                         :type      "ROLE"
+                         :right     "VIEW"}]})
+
+(def valid-entry
+  {:name "valid"
+   :description "valid machine image"
+   :state "CREATING"
+   :type "SNAPSHOT"
+   :imageLocation "http://example.com/image"
+   :relatedImage {:href "MachineImage/other-uuid"}})
+
+
+(deftest lifecycle
+
+  ;; anonymous create should fail
+  (-> (session (ring-app))
+      (request base-uri
+               :request-method :post
+               :body (json/write-str valid-entry))
+      (t/is-status 403))
+
+  ;; anonymous query should also fail
+  (-> (session (ring-app))
+      (request base-uri)
+      (t/is-status 403))
+
+  ;; add a new entry
+  (let [uri (-> (session (ring-app))
+                (authorize "jane" "user_password")
+                (request base-uri
+                         :request-method :post
+                         :body (json/write-str valid-entry))
+                (t/is-status 201)
+                (t/location))
+        abs-uri (str c/service-context uri)]
+
+    (is uri)
+
+    ;; verify that the new entry is accessible
+    (-> (session (ring-app))
+        (authorize "jane" "user_password")
+        (request abs-uri)
+        (t/is-status 200)
+        (dissoc :acl)                                       ;; ACL added automatically
+        (t/does-body-contain valid-entry))
+
+    ;; query to see that entry is listed
+    (let [entries (-> (session (ring-app))
+                      (authorize "jane" "user_password")
+                      (request base-uri)
+                      (t/is-status 200)
+                      (t/is-resource-uri collection-uri)
+                      (t/is-count pos?)
+                      (t/entries :machineImages))]
+      (is ((set (map :id entries)) uri)))
+
+    ;; delete the entry
+    (-> (session (ring-app))
+        (authorize "jane" "user_password")
+        (request abs-uri
+                 :request-method :delete)
+        (t/is-status 200))
+
+    ;; ensure that it really is gone
+    (-> (session (ring-app))
+        (authorize "jane" "user_password")
+        (request abs-uri)
+        (t/is-status 404))))
+
+(deftest bad-methods
+  (let [resource-uri (str base-uri "/" (u/random-uuid))]
+    (doall
+      (for [[uri method] [[base-uri :options]
+                          [base-uri :delete]
+                          [base-uri :put]
+                          [resource-uri :options]
+                          [resource-uri :post]]]
+        (do
+          (-> (session (ring-app))
+              (request uri
+                       :request-method method
+                       :body (json/write-str {:dummy "value"}))
+              (t/is-status 405)))))))
